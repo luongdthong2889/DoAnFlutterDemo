2635a2c95f5ceb340bb4d4ce08b5399f8e6c49d9
luongdthong2889 <luong.dthong@gmail.com>
2022-04-27 00:27:56 +0700

    first commit

---
+++ fitness_flutter/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fitness_flutter/core/const/color_constants.dart';
+import 'package:fitness_flutter/screens/onboarding/page/onboarding_page.dart';
+import 'package:fitness_flutter/screens/tab_bar/page/tab_bar_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+void main() async {
+ WidgetsFlutterBinding.ensureInitialized();
+ SystemChrome.setPreferredOrientations([
+   DeviceOrientation.portraitUp,
+   DeviceOrientation.portraitDown,
+ ]);
+ await Firebase.initializeApp();
+ runApp(MyApp());
+}
+ 
+class MyApp extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   final isLoggedIn = FirebaseAuth.instance.currentUser != null;
+ 
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     title: 'Fitness',
+     theme: ThemeData(
+       textTheme:
+           TextTheme(bodyText1: TextStyle(color: ColorConstants.textColor)),
+       fontFamily: 'NotoSansKR',
+       scaffoldBackgroundColor: Colors.white,
+       visualDensity: VisualDensity.adaptivePlatformDensity,
+     ),
+     home: isLoggedIn ? TabBarPage() : OnboardingPage(),
+   );
+ }
+}

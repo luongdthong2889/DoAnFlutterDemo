069aeb606a44bbf3427eb688fe283dc0c0265988
luongdthong2889 <luong.dthong@gmail.com>
2022-04-27 03:24:03 +0700

    done part 1
    done part 1

--- fitness_flutter/lib/core/const/global_constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:fitness_flutter/data/user_data.dart';
-import 'package:fitness_flutter/data/workout_data.dart';
-
-class GlobalConstants {
-  static late UserData currentUser;
-  static late List<WorkoutData> workouts;
-}
